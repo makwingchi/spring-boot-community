dba363feb88e23358bfb145a7b826649a7585054
Wingchi Mak <makwin9chi@gmail.com>
2020-04-20 23:52:47 -0400

    test redis hyperloglog and bitmap

--- src/test/java/com/project/community/RedisTests.java
+++ src/test/java/com/project/community/RedisTests.java
@@ -5,10 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.core.BoundValueOperations;
-import org.springframework.data.redis.core.RedisOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisStringCommands;
+import org.springframework.data.redis.core.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -18,14 +17,14 @@ import java.util.concurrent.TimeUnit;
  * @create 2020-04-18 17:07
  */
 @SpringBootTest
-public class RedisTests {
+class RedisTests {
 
     @Qualifier("redisTemplate")
     @Autowired
     private RedisTemplate template;
 
     @Test
-    public void testStrings() {
+    void testStrings() {
         String redisKey = "test:count";
 
         template.opsForValue().set(redisKey, 1);
@@ -34,7 +33,7 @@ public class RedisTests {
     }
 
     @Test
-    public void testHashes() {
+    void testHashes() {
         String redisKey = "test:user";
 
         template.opsForHash().put(redisKey, "id", 1);
@@ -45,7 +44,7 @@ public class RedisTests {
     }
 
     @Test
-    public void testLists() {
+    void testLists() {
         String redisKey = "test:ids";
 
         template.opsForList().leftPush(redisKey, 101);
@@ -59,7 +58,7 @@ public class RedisTests {
     }
 
     @Test
-    public void testSets() {
+    void testSets() {
         String redisKey = "test:teachers";
 
         template.opsForSet().add(redisKey, "tom", "and", "jerry");
@@ -69,7 +68,7 @@ public class RedisTests {
     }
 
     @Test
-    public void testSortedSets() {
+    void testSortedSets() {
         String redisKey = "test:students";
 
         template.opsForZSet().add(redisKey, "tom", 1.0);
@@ -83,7 +82,7 @@ public class RedisTests {
     }
 
     @Test
-    public void testKeys() {
+    void testKeys() {
         template.delete("test:user");
         System.out.println(template.hasKey("test:user"));
 
@@ -92,7 +91,7 @@ public class RedisTests {
 
     // access the same key
     @Test
-    public void testBoundOperations() {
+    void testBoundOperations() {
         String redisKey = "test:count";
         BoundValueOperations operations = template.boundValueOps(redisKey);
         operations.increment();
@@ -106,7 +105,7 @@ public class RedisTests {
 
     // Programmatic Tx
     @Test
-    public void testTransaction() {
+    void testTransaction() {
         Object object = template.execute(new SessionCallback() {
             @Override
             public Object execute(RedisOperations redisOperations) throws DataAccessException {
@@ -124,6 +123,92 @@ public class RedisTests {
         });
 
         System.out.println(object);
+    }
+
+    // unique values of 200K numbers
+    @Test
+    void testHyperLogLog() {
+        String redisKey = "test:hll:01";
+        for (int i = 1; i <= 100000; i++) {
+            template.opsForHyperLogLog().add(redisKey, i);
+        }
+
+        for (int i = 1; i <= 100000; i++) {
+            int r = (int) Math.random() * 100000 + 1;
+            template.opsForHyperLogLog().add(redisKey, r);
+        }
+
+        Long size = template.opsForHyperLogLog().size(redisKey);
+        System.out.println(size);
+    }
+
+    @Test
+    void testHyperLogLogUnion() {
+        String redisKey1 = "test:hll:02";
+        for (int i = 1; i <= 10000; i++) {
+            template.opsForHyperLogLog().add(redisKey1, i);
+        }
+
+        String redisKey2 = "test:hll:02";
+        for (int i = 5001; i <= 15000; i++) {
+            template.opsForHyperLogLog().add(redisKey2, i);
+        }
+
+        String redisKey3 = "test:hll:02";
+        for (int i = 10001; i <= 20000; i++) {
+            template.opsForHyperLogLog().add(redisKey3, i);
+        }
+
+        String unionKey = "test:hll:union";
+        template.opsForHyperLogLog().union(unionKey, redisKey1, redisKey2, redisKey3);
+
+        Long size = template.opsForHyperLogLog().size(unionKey);
+        System.out.println(size);
+    }
+
+    @Test
+    void testBitMap() {
+        String redisKey = "test:bm:01";
+
+        template.opsForValue().setBit(redisKey, 1, true);
+        template.opsForValue().setBit(redisKey, 4, true);
+        template.opsForValue().setBit(redisKey, 7, true);
+
+        System.out.println(template.opsForValue().getBit(redisKey, 0));
+        System.out.println(template.opsForValue().getBit(redisKey, 1));
+        System.out.println(template.opsForValue().getBit(redisKey, 2));
+
+        Object obj = template.execute((RedisCallback) conn -> conn.bitCount(redisKey.getBytes()));
+        System.out.println(obj);
+    }
+
+    @Test
+    void testOr() {
+        String redisKey2 = "test:bm:02";
+        template.opsForValue().setBit(redisKey2, 0, true);
+        template.opsForValue().setBit(redisKey2, 1, true);
+        template.opsForValue().setBit(redisKey2, 2, true);
+
+        String redisKey3 = "test:bm:03";
+        template.opsForValue().setBit(redisKey3, 2, true);
+        template.opsForValue().setBit(redisKey3, 3, true);
+        template.opsForValue().setBit(redisKey3, 4, true);
+
+        String redisKey4 = "test:bm:04";
+        template.opsForValue().setBit(redisKey4, 4, true);
+        template.opsForValue().setBit(redisKey4, 5, true);
+        template.opsForValue().setBit(redisKey4, 6, true);
+
+        String redisKey = "test:bm:or";
+        Object obj = template.execute((RedisCallback) conn -> {
+            conn.bitOp(RedisStringCommands.BitOperation.OR,
+                    redisKey.getBytes(), redisKey2.getBytes(),
+                    redisKey3.getBytes(), redisKey4.getBytes());
+            return conn.bitCount(redisKey.getBytes());
+        });
+
+        System.out.println(obj);
+
     }
 
 }
